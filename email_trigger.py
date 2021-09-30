b95545221c23da10f10187eec5e39d591e31b405
Hariharan (H.) Macheri Kumar <hari.haran.mk@gmail.com>
2021-09-30 07:42:34 +0200

    modified with message

--- email_trigger.py
+++ email_trigger.py
@@ -1,11 +1,17 @@
+
 import smtplib, ssl
 
 port = 465  # For SSL
+smtp_server = "smtp.gmail.com"
+sender_email = "reminder.sampproject@gmail.com"  # Enter your address
+receiver_email = "hari.haran.mk@gmail.com"  # Enter receiver address
 password = input("Type your password and press enter: ")
+message = """\
+Subject: Hi there
 
-# Create a secure SSL context
+This message is sent from Python."""
+
 context = ssl.create_default_context()
-
-with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
-    server.login("reminder.sampproject@gmail.com", password)
-    # TODO: Send email here
+with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+    server.login(sender_email, password)
+    server.sendmail(sender_email, receiver_email, message)
